9e08aa674140d436d4b8a44a24543a4eb0c41d19
Marcos Lobo <marcos.lobo.nit@gmail.com>
2020-08-23 17:29:58 -0300

    teste

---
+++ src/Actions.PRCheck/Models/HithubEventoAction.cs
@@ -0,0 +1,390 @@
+﻿
+    using System;
+    using System.Collections.Generic;
+
+    using System.Globalization;
+    using Newtonsoft.Json;
+    using Newtonsoft.Json.Converters;
+namespace Actions.PRCheck
+{
+    public partial class GitHubActionModelInside
+    {
+        [JsonProperty("after")]
+        public string After { get; set; }
+
+        [JsonProperty("base_ref")]
+        public object BaseRef { get; set; }
+
+        [JsonProperty("before")]
+        public string Before { get; set; }
+
+        [JsonProperty("commits")]
+        public Commit[] Commits { get; set; }
+
+        [JsonProperty("compare")]
+        public Uri Compare { get; set; }
+
+        [JsonProperty("created")]
+        public bool Created { get; set; }
+
+        [JsonProperty("deleted")]
+        public bool Deleted { get; set; }
+
+        [JsonProperty("forced")]
+        public bool Forced { get; set; }
+
+        [JsonProperty("head_commit")]
+        public Commit HeadCommit { get; set; }
+
+        [JsonProperty("pusher")]
+        public Pusher Pusher { get; set; }
+
+        [JsonProperty("ref")]
+        public string Ref { get; set; }
+
+        [JsonProperty("repository")]
+        public Repository Repository { get; set; }
+
+        [JsonProperty("sender")]
+        public Sender Sender { get; set; }
+    }
+
+    public partial class Commit
+    {
+        [JsonProperty("author")]
+        public Author Author { get; set; }
+
+        [JsonProperty("committer")]
+        public Author Committer { get; set; }
+
+        [JsonProperty("distinct")]
+        public bool Distinct { get; set; }
+
+        [JsonProperty("id")]
+        public string Id { get; set; }
+
+        [JsonProperty("message")]
+        public string Message { get; set; }
+
+        [JsonProperty("timestamp")]
+        public DateTimeOffset Timestamp { get; set; }
+
+        [JsonProperty("tree_id")]
+        public string TreeId { get; set; }
+
+        [JsonProperty("url")]
+        public Uri Url { get; set; }
+    }
+
+    public partial class Author
+    {
+        [JsonProperty("email")]
+        public string Email { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("username")]
+        public string Username { get; set; }
+    }
+
+    public partial class Pusher
+    {
+        [JsonProperty("email")]
+        public string Email { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+    }
+
+    public partial class Repository
+    {
+        [JsonProperty("archive_url")]
+        public string ArchiveUrl { get; set; }
+
+        [JsonProperty("archived")]
+        public bool Archived { get; set; }
+
+        [JsonProperty("assignees_url")]
+        public string AssigneesUrl { get; set; }
+
+        [JsonProperty("blobs_url")]
+        public string BlobsUrl { get; set; }
+
+        [JsonProperty("branches_url")]
+        public string BranchesUrl { get; set; }
+
+        [JsonProperty("clone_url")]
+        public Uri CloneUrl { get; set; }
+
+        [JsonProperty("collaborators_url")]
+        public string CollaboratorsUrl { get; set; }
+
+        [JsonProperty("comments_url")]
+        public string CommentsUrl { get; set; }
+
+        [JsonProperty("commits_url")]
+        public string CommitsUrl { get; set; }
+
+        [JsonProperty("compare_url")]
+        public string CompareUrl { get; set; }
+
+        [JsonProperty("contents_url")]
+        public string ContentsUrl { get; set; }
+
+        [JsonProperty("contributors_url")]
+        public Uri ContributorsUrl { get; set; }
+
+        [JsonProperty("created_at")]
+        public long CreatedAt { get; set; }
+
+        [JsonProperty("default_branch")]
+        public string DefaultBranch { get; set; }
+
+        [JsonProperty("deployments_url")]
+        public Uri DeploymentsUrl { get; set; }
+
+        [JsonProperty("description")]
+        public object Description { get; set; }
+
+        [JsonProperty("disabled")]
+        public bool Disabled { get; set; }
+
+        [JsonProperty("downloads_url")]
+        public Uri DownloadsUrl { get; set; }
+
+        [JsonProperty("events_url")]
+        public Uri EventsUrl { get; set; }
+
+        [JsonProperty("fork")]
+        public bool Fork { get; set; }
+
+        [JsonProperty("forks")]
+        public long Forks { get; set; }
+
+        [JsonProperty("forks_count")]
+        public long ForksCount { get; set; }
+
+        [JsonProperty("forks_url")]
+        public Uri ForksUrl { get; set; }
+
+        [JsonProperty("full_name")]
+        public string FullName { get; set; }
+
+        [JsonProperty("git_commits_url")]
+        public string GitCommitsUrl { get; set; }
+
+        [JsonProperty("git_refs_url")]
+        public string GitRefsUrl { get; set; }
+
+        [JsonProperty("git_tags_url")]
+        public string GitTagsUrl { get; set; }
+
+        [JsonProperty("git_url")]
+        public string GitUrl { get; set; }
+
+        [JsonProperty("has_downloads")]
+        public bool HasDownloads { get; set; }
+
+        [JsonProperty("has_issues")]
+        public bool HasIssues { get; set; }
+
+        [JsonProperty("has_pages")]
+        public bool HasPages { get; set; }
+
+        [JsonProperty("has_projects")]
+        public bool HasProjects { get; set; }
+
+        [JsonProperty("has_wiki")]
+        public bool HasWiki { get; set; }
+
+        [JsonProperty("homepage")]
+        public object Homepage { get; set; }
+
+        [JsonProperty("hooks_url")]
+        public Uri HooksUrl { get; set; }
+
+        [JsonProperty("html_url")]
+        public Uri HtmlUrl { get; set; }
+
+        [JsonProperty("id")]
+        public long Id { get; set; }
+
+        [JsonProperty("issue_comment_url")]
+        public string IssueCommentUrl { get; set; }
+
+        [JsonProperty("issue_events_url")]
+        public string IssueEventsUrl { get; set; }
+
+        [JsonProperty("issues_url")]
+        public string IssuesUrl { get; set; }
+
+        [JsonProperty("keys_url")]
+        public string KeysUrl { get; set; }
+
+        [JsonProperty("labels_url")]
+        public string LabelsUrl { get; set; }
+
+        [JsonProperty("language")]
+        public object Language { get; set; }
+
+        [JsonProperty("languages_url")]
+        public Uri LanguagesUrl { get; set; }
+
+        [JsonProperty("license")]
+        public object License { get; set; }
+
+        [JsonProperty("master_branch")]
+        public string MasterBranch { get; set; }
+
+        [JsonProperty("merges_url")]
+        public Uri MergesUrl { get; set; }
+
+        [JsonProperty("milestones_url")]
+        public string MilestonesUrl { get; set; }
+
+        [JsonProperty("mirror_url")]
+        public object MirrorUrl { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("node_id")]
+        public string NodeId { get; set; }
+
+        [JsonProperty("notifications_url")]
+        public string NotificationsUrl { get; set; }
+
+        [JsonProperty("open_issues")]
+        public long OpenIssues { get; set; }
+
+        [JsonProperty("open_issues_count")]
+        public long OpenIssuesCount { get; set; }
+
+        [JsonProperty("owner")]
+        public Sender Owner { get; set; }
+
+        [JsonProperty("private")]
+        public bool Private { get; set; }
+
+        [JsonProperty("pulls_url")]
+        public string PullsUrl { get; set; }
+
+        [JsonProperty("pushed_at")]
+        public long PushedAt { get; set; }
+
+        [JsonProperty("releases_url")]
+        public string ReleasesUrl { get; set; }
+
+        [JsonProperty("size")]
+        public long Size { get; set; }
+
+        [JsonProperty("ssh_url")]
+        public string SshUrl { get; set; }
+
+        [JsonProperty("stargazers")]
+        public long Stargazers { get; set; }
+
+        [JsonProperty("stargazers_count")]
+        public long StargazersCount { get; set; }
+
+        [JsonProperty("stargazers_url")]
+        public Uri StargazersUrl { get; set; }
+
+        [JsonProperty("statuses_url")]
+        public string StatusesUrl { get; set; }
+
+        [JsonProperty("subscribers_url")]
+        public Uri SubscribersUrl { get; set; }
+
+        [JsonProperty("subscription_url")]
+        public Uri SubscriptionUrl { get; set; }
+
+        [JsonProperty("svn_url")]
+        public Uri SvnUrl { get; set; }
+
+        [JsonProperty("tags_url")]
+        public Uri TagsUrl { get; set; }
+
+        [JsonProperty("teams_url")]
+        public Uri TeamsUrl { get; set; }
+
+        [JsonProperty("trees_url")]
+        public string TreesUrl { get; set; }
+
+        [JsonProperty("updated_at")]
+        public DateTimeOffset UpdatedAt { get; set; }
+
+        [JsonProperty("url")]
+        public Uri Url { get; set; }
+
+        [JsonProperty("watchers")]
+        public long Watchers { get; set; }
+
+        [JsonProperty("watchers_count")]
+        public long WatchersCount { get; set; }
+    }
+
+    public partial class Sender
+    {
+        [JsonProperty("avatar_url")]
+        public Uri AvatarUrl { get; set; }
+
+        [JsonProperty("email", NullValueHandling = NullValueHandling.Ignore)]
+        public string Email { get; set; }
+
+        [JsonProperty("events_url")]
+        public string EventsUrl { get; set; }
+
+        [JsonProperty("followers_url")]
+        public Uri FollowersUrl { get; set; }
+
+        [JsonProperty("following_url")]
+        public string FollowingUrl { get; set; }
+
+        [JsonProperty("gists_url")]
+        public string GistsUrl { get; set; }
+
+        [JsonProperty("gravatar_id")]
+        public string GravatarId { get; set; }
+
+        [JsonProperty("html_url")]
+        public Uri HtmlUrl { get; set; }
+
+        [JsonProperty("id")]
+        public long Id { get; set; }
+
+        [JsonProperty("login")]
+        public string Login { get; set; }
+
+        [JsonProperty("name", NullValueHandling = NullValueHandling.Ignore)]
+        public string Name { get; set; }
+
+        [JsonProperty("node_id")]
+        public string NodeId { get; set; }
+
+        [JsonProperty("organizations_url")]
+        public Uri OrganizationsUrl { get; set; }
+
+        [JsonProperty("received_events_url")]
+        public Uri ReceivedEventsUrl { get; set; }
+
+        [JsonProperty("repos_url")]
+        public Uri ReposUrl { get; set; }
+
+        [JsonProperty("site_admin")]
+        public bool SiteAdmin { get; set; }
+
+        [JsonProperty("starred_url")]
+        public string StarredUrl { get; set; }
+
+        [JsonProperty("subscriptions_url")]
+        public Uri SubscriptionsUrl { get; set; }
+
+        [JsonProperty("type")]
+        public string Type { get; set; }
+
+        [JsonProperty("url")]
+        public Uri Url { get; set; }
+    }
+}
--- src/Actions.PRCheck/Program.cs
+++ src/Actions.PRCheck/Program.cs
@@ -12,22 +12,22 @@ namespace Actions.PRCheck
         static async Task Main(string[] args)
         {
 
-            using (var client = new HttpClient())
-            {
-                client.DefaultRequestHeaders.Add("Accept", "application/vnd.github.v3+json");
-                client.DefaultRequestHeaders.Add("User-Agent", "request");
+            //using (var client = new HttpClient())
+            //{
+            //    client.DefaultRequestHeaders.Add("Accept", "application/vnd.github.v3+json");
+            //    client.DefaultRequestHeaders.Add("User-Agent", "request");
 
-                var content = await client.GetStringAsync("https://api.github.com/repos/prefeiturasp/SME-NovoSGP/pulls?state=open");
-                var a = JsonConvert.DeserializeObject<List<GitHubModels>>(content);
-            }
+            //    var content = await client.GetStringAsync("https://api.github.com/repos/prefeiturasp/SME-NovoSGP/pulls?state=open");
+            //    var a = JsonConvert.DeserializeObject<List<GitHubModels>>(content);
+            //}
 
 
             //GITHUB_HEAD_REF - e GITHUB_BASE_REF
 
-            Console.Out.WriteLine($"GITHUB_HEAD_REF -> {Environment.GetEnvironmentVariable("GITHUB_HEAD_REF")}");
-            Console.Out.WriteLine($"GITHUB_BASE_REF -> {Environment.GetEnvironmentVariable("GITHUB_BASE_REF")}");
-            Console.Out.WriteLine($"BRANCH_REF -> {Environment.GetEnvironmentVariable("BRANCH_REF")}");
-            Console.Out.WriteLine($"GITHUB_EVENT_PATH -> {Environment.GetEnvironmentVariable("GITHUB_EVENT_PATH")}");
+            //Console.Out.WriteLine($"GITHUB_HEAD_REF -> {Environment.GetEnvironmentVariable("GITHUB_HEAD_REF")}");
+            //Console.Out.WriteLine($"GITHUB_BASE_REF -> {Environment.GetEnvironmentVariable("GITHUB_BASE_REF")}");
+            //Console.Out.WriteLine($"BRANCH_REF -> {Environment.GetEnvironmentVariable("BRANCH_REF")}");
+            //Console.Out.WriteLine($"GITHUB_EVENT_PATH -> {Environment.GetEnvironmentVariable("GITHUB_EVENT_PATH")}");
 
 
             using (StreamReader r = new StreamReader(Environment.GetEnvironmentVariable("GITHUB_EVENT_PATH")))
@@ -35,12 +35,16 @@ namespace Actions.PRCheck
                 string json = r.ReadToEnd();
 
                 Console.Out.WriteLine(json);
-                
+
+                var a = JsonConvert.DeserializeObject<List<GitHubActionModelInside>>(json);
+
 
             }
 
 
-            Console.Out.WriteLine($"ARGS -> {string.Join(" ", args)}");
+
+
+            //Console.Out.WriteLine($"ARGS -> {string.Join(" ", args)}");
 
         }
     }
